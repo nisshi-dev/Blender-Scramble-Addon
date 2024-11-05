807c902f3356797492482a6a5a104901cfc246c0
Yoshikatsu Nishida <mail@nishida.dev>
2024-11-05 12:14:03 +0900

    アドオンのバージョン番号を更新

--- __init__.py
+++ __init__.py
@@ -11,7 +11,7 @@ from bpy.props import *
 bl_info = {
 	"name" : "Scramble Addon",
 	"author" : "Saidenka, Bookyakuno, nikogoli",
-	"version" : (1, 1, 2),
+	"version" : (1, 1, 2, 1),
 	"blender" : (4, 2, 0),
 	"location" : "End of a varied menu",
 	"description" : "Assortment of extended functions of saidenka\'s production",
